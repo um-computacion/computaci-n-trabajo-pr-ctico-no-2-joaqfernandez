b1f5b6293efd6b0a3cf02daac92380f0bb2a4d68
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-04-15 20:02:30 -0300

    agrego interaccion por consola

--- src/palindrome.py
+++ src/palindrome.py
@@ -16,3 +16,16 @@ def limpiar_texto(texto):
 
 def comparar(texto_limpio):
     return texto_limpio == texto_limpio[::-1]
+
+if __name__ == "__main__":
+    try:
+        print("Verificador de palindromos")
+        print("Ctrl+C para salir")
+        while True:
+            entrada = input("Ingresa una palabra o frase: ")
+            if is_palindrome(entrada):
+                print("Es un palíndromo")
+            else:
+                print("No es un palíndromo")
+    except KeyboardInterrupt:
+        print("Programa terminado. ¡Hasta luego!")
